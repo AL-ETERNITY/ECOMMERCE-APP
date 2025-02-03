85e0e6e193cd540cf7d939bbede8d08e9f3a96b5
Siddhant Shah <siddhantshah469@gmail.com>
2025-02-04 02:40:27 +0530

    feat(App): add routing for add and list pages in the admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
+import {Routes, Route} from 'react-router-dom'
+import add from './pages/add'
+import list from './pages/list'
 
 const App = () => {
   return (
@@ -10,6 +13,13 @@ const App = () => {
         <hr/>
         <div className='flex w-full'>
           <Sidebar />
+          <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base '>
+            <Routes>
+              <Route path='/add' element={<add/>} />
+              <Route path='/list' element={<list/>} />
+              <Route path='/orders' element={<orders/>} />
+            </Routes>
+          </div>
         </div>
       </>
     </div>
