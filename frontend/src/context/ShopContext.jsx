59141511f4b657eeed6ba59e2d4f66d5eec7beab
Siddhant Shah <siddhantshah469@gmail.com>
2025-02-07 19:10:21 +0530

    feat(Navbar, ShopContext, Login): implement token management for authentication; add logout functionality and enhance login flow

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -14,6 +14,7 @@ const ShopContextProvider = ({ children }) => {
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
     const [products, setProducts] = useState([]);
+    const [token, setToken] = useState('');
     const navigate = useNavigate();
 
     const addToCart = async (itemId, size) => {
@@ -94,13 +95,19 @@ const ShopContextProvider = ({ children }) => {
         getProductsData();
     }, []);
 
+    useEffect(()=>{
+        if(!token && localStorage.getItem('token')){
+            setToken(localStorage.getItem('token'));
+        }
+    })
+
     const value = {
         products, currency, delivery_fee,
         search, setSearch, showSearch, setShowSearch,
         cartItems, addToCart,
         getCartCount, updateQuantity,
         getCartAmount, 
-        navigate,backendUrl
+        navigate,backendUrl, setToken, token
     }
     
     return (
